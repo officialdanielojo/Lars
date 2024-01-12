de09831df5fb3d62992ebabaa3c5c110df75ae90
Ojo Daniel <danielojo237@gmail.com>
2024-01-12 19:05:30 +0100

    Lars Project

---
+++ javascript.js
@@ -0,0 +1,16 @@
+const closeNav = document.querySelector(".close_nav");
+const openNav = document.querySelector(".bar_con");
+const nav = document.querySelector(".nav_section");
+const logo = document.querySelector(".logo");
+
+closeNav.addEventListener("click", function () {
+  nav.classList.remove("showNav");
+});
+
+openNav.addEventListener("click", function () {
+  nav.classList.add("showNav");
+});
+
+logo.addEventListener("click", function () {
+  location.href = "/index.html";
+});
